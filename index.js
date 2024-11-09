b27ca8662c3c9f7bde9bf298a2469f130540d5b5
robert koopedi <koopedi05@gmail.com>
2024-11-09 17:22:40 +0200

    updated flavour selectiong and stock check

--- index.js
+++ index.js
@@ -5,21 +5,23 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+console.log(userPreference = `Sugar-free` ? "Sugar-free" : "Regular");
+
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
-let cansLeft = 3; // Any number of cans
-console.log(``);
+let cansLeft = 5; // Any number of cans
+console.log(cansLeft < `5` ? "Time to restock!" : "We're stocked!");
+
 
 
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
-let heartRate = 95; // Current heart rate in bpm
+let heartRate = 100; // Current heart rate in bpm
 console.log(`'}`);
 
 
@@ -27,8 +29,7 @@ console.log(`'}`);
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
-let currentTemp = 4; // Current temperature in °C
-console.log(``);
+let currentTemp = 1; // Current temperature in °C
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -37,12 +38,12 @@ console.log(``);
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22; 
+let currentHour = 3;
+
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
-
 
 
 
